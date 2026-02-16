7dd43cbe408ed8c780b3eee6432fb7d95a926512
Caleb Chiang <calebchiang1002@gmail.com>
2026-02-16 13:11:18 -0800

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Caleb Chiang on 2026-02-16.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ notesTests/notesTests.swift
@@ -0,0 +1,17 @@
+//
+//  notesTests.swift
+//  notesTests
+//
+//  Created by Caleb Chiang on 2026-02-16.
+//
+
+import Testing
+@testable import notes
+
+struct notesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
